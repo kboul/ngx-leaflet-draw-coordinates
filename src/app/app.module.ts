2c903c4644097c66f0e9781ee4cc1afd8e13cca6
kboul <kos.voulgaridis@gmail.com>
2018-10-04 17:17:39 +0300

    Import leaflet.css, leaflet-draw.css, leaflet.js on angular.json & also leaflet, leaflet-draw on app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { LeafletDrawModule } from '@asymmetrik/ngx-leaflet-draw';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+      	AppComponent
+    ],
+    imports: [
+		BrowserModule,
+		LeafletModule.forRoot(),
+		LeafletDrawModule.forRoot()
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
