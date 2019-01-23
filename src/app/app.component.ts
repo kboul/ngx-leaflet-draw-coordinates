af1bb94f0f233a29398a4b66df133ed162a417fb
kboul <kos.voulgaridis@gmail.com>
2019-01-24 00:07:18 +0200

    Fix some indentation on app.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,16 @@ import 'leaflet/dist/images/marker-shadow.png';
 import 'leaflet/dist/images/marker-icon.png';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	map: L.Map;
+    map: L.Map;
 
     options = {
         layers: [
-          tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+            tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
         ],
         zoom: 15,
         center: latLng(8.524139, 76.936638)
@@ -23,28 +23,28 @@ export class AppComponent {
 
     drawOptions = {
         position: 'topleft',
-		draw: {
-			marker: {
-				icon: L.icon({
-					iconSize: [ 25, 41 ],
-					iconAnchor: [ 13, 41 ],
-					iconUrl: '../../assets/marker-icon.png',
-					shadowUrl: '../../assets/marker-shadow.png'
-				})
-			}
-		}
+        draw: {
+            marker: {
+                icon: L.icon({
+                    iconSize: [25, 41],
+                    iconAnchor: [13, 41],
+                    iconUrl: '../../assets/marker-icon.png',
+                    shadowUrl: '../../assets/marker-shadow.png'
+                })
+            }
+        }
     };
 
-  	onMapReady(map: Map) {
-		map.on(L.Draw.Event.CREATED, function (e) {
-			const type = (e as any).layerType,
-			layer = (e as any).layer;
+    onMapReady(map: Map) {
+        map.on(L.Draw.Event.CREATED, function (e) {
+            const type = (e as any).layerType,
+                layer = (e as any).layer;
 
-			if (type === 'polygon') {
-				// here you got the polygon coordinates
-				const polygonCoordinates = layer._latlngs;
-				console.log(polygonCoordinates);
-			}
-		});
-  	}
+            if (type === 'polygon') {
+                // here you got the polygon coordinates
+                const polygonCoordinates = layer._latlngs;
+                console.log(polygonCoordinates);
+            }
+        });
+    }
 }
