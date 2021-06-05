b3bea8a9cf7587e55150f5b8cd7c06bc658f8d07
kboul <kos.voulgaridis@gmail.com>
2021-06-05 15:53:44 +0300

    Upgrade to Angular 12

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,39 @@
 import { TestBed, waitForAsync } from "@angular/core/testing";
 import { AppComponent } from "./app.component";
+
 describe("AppComponent", () => {
-	beforeEach(waitForAsync(() => {
-		TestBed.configureTestingModule({
-			declarations: [AppComponent]
-		}).compileComponents();
-	}));
-	it("should create the app", waitForAsync(() => {
-		const fixture = TestBed.createComponent(AppComponent);
-		const app = fixture.debugElement.componentInstance;
-		expect(app).toBeTruthy();
-	}));
-	it(`should have as title 'ngx-leaflet-draw-coordinates'`, waitForAsync(() => {
-		const fixture = TestBed.createComponent(AppComponent);
-		const app = fixture.debugElement.componentInstance;
-		expect(app.title).toEqual("ngx-leaflet-draw-coordinates");
-	}));
-	it("should render title in a h1 tag", waitForAsync(() => {
-		const fixture = TestBed.createComponent(AppComponent);
-		fixture.detectChanges();
-		const compiled = fixture.debugElement.nativeElement;
-		expect(compiled.querySelector("h1").textContent).toContain(
-			"Welcome to ngx-leaflet-draw-coordinates!"
-		);
-	}));
+	beforeEach(
+		waitForAsync(() => {
+			TestBed.configureTestingModule({
+				declarations: [AppComponent]
+			}).compileComponents();
+		})
+	);
+	it(
+		"should create the app",
+		waitForAsync(() => {
+			const fixture = TestBed.createComponent(AppComponent);
+			const app = fixture.debugElement.componentInstance;
+			expect(app).toBeTruthy();
+		})
+	);
+	it(
+		`should have as title 'ngx-leaflet-draw-coordinates'`,
+		waitForAsync(() => {
+			const fixture = TestBed.createComponent(AppComponent);
+			const app = fixture.debugElement.componentInstance;
+			expect(app.title).toEqual("ngx-leaflet-draw-coordinates");
+		})
+	);
+	it(
+		"should render title in a h1 tag",
+		waitForAsync(() => {
+			const fixture = TestBed.createComponent(AppComponent);
+			fixture.detectChanges();
+			const compiled = fixture.debugElement.nativeElement;
+			expect(compiled.querySelector("h1").textContent).toContain(
+				"Welcome to ngx-leaflet-draw-coordinates!"
+			);
+		})
+	);
 });
